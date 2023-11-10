44ef2c56713e58ba3304307da5fb73c0c4c35c6d
isabella <cisabella626@gmail.com>
2023-11-09 22:46:45 -0300

    lacos3

--- CargoSwitch6.java
+++ CargoSwitch6.java
@@ -1,8 +1,81 @@
 package atividade2;
 
+import java.util.Scanner;
+
 public class CargoSwitch6 {
 
 	public static void main(String[] args) {
+		
+		String nome, cargo;
+		int codigoCargo;
+		float sal, reajusteSa, novoSal;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("\nNome do colaborador: ");
+	    nome = leia.nextLine();
+		
+		System.out.println("\nEntre com um numero inteiro de 1 a 6 para o Cargo: ");
+		codigoCargo = leia.nextInt();
+		
+		System.out.println("\nSalário: ");
+		sal = leia.nextFloat();
+		
+		
+		
+		
+		switch (codigoCargo) {
+		
+		case 1:
+			cargo = "gerente";
+					reajusteSa = 0.10f;	
+					novoSal = sal + (reajusteSa  * sal);
+					System.out.printf("Nome do Colaborador: %s \nCargo: Gerente \nSalário: %.2f", cargo, novoSal);
+			break;
+		
+		case 2:
+			cargo = "Vendedor";
+					reajusteSa = 0.07f;
+					novoSal = sal + (reajusteSa  * sal);
+					System.out.printf("Nome do Colaborador: %s \nCargo: Gerente \nSalário: %.2f", cargo, novoSal);
+			break;
+		
+		case 3:
+			cargo = "Supervisor";
+					reajusteSa = 0.09f;
+					novoSal = sal + (reajusteSa  * sal);
+					System.out.printf("Nome do Colaborador: %s \nCargo: Gerente \nSalário: %.2f", cargo, novoSal);
+			break;
+		
+		case 4:
+			cargo = "Motorista";
+					reajusteSa = 0.06f;
+					novoSal = sal + (reajusteSa  * sal);
+					System.out.printf("Nome do Colaborador: %s \nCargo: Gerente \nSalário: %.2f", cargo, novoSal);
+			break;
+		
+		case 5:
+			cargo = "Estoquista";
+					reajusteSa = 0.05f;
+					novoSal = sal + (reajusteSa  * sal);
+					System.out.printf("Nome do Colaborador: %s \nCargo: Gerente \nSalário: %.2f", cargo, novoSal);
+			break;
+		
+		case 6:
+			cargo = "Técnico de TI";
+					reajusteSa = 0.08f;
+					novoSal = sal + (reajusteSa  * sal);
+					System.out.printf("Nome do Colaborador: %s \nCargo: Gerente \nSalário: %.2f", cargo, novoSal);
+			break;
+			
+			
+					
+		}
+		
+		
+		
+		
+		
 		
 
 	}
--- ImparParIf2.java
+++ ImparParIf2.java
@@ -1,9 +1,31 @@
 package atividade2;
 
+import java.util.Scanner;
+
 public class ImparParIf2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int n1;
+
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Digite um número inteiro: ");
+		n1 = leia.nextInt();
+
+		if (n1 % 2 == 0) {
+			if (n1 > 0) {
+				System.out.println("O número é " + n1 + " é par e positivo ");
+			} else {
+				System.out.println("O número é " + n1 + " é par e negativo ");
+			}
+		} else {
+			if (n1 > 0) {
+				System.out.println("O número é " + n1 + " é impar e positivo ");
+			} else {
+				System.out.println("O número é " + n1 + " é impar e negativo ");
+			}
+		}
 
 	}
 
